7fb284609cd62c00eb6e8bfdbe3191b83079b317
James Lang <hi@thejameslang.com>
2023-08-19 19:51:12 -0500

    Conditionally Allow Movement using react-beautiful-dnd Draggable and Droppable Props

--- src/column.jsx
+++ src/column.jsx
@@ -28,7 +28,11 @@ export default class Column extends React.Component {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
-        <Droppable droppableId={this.props.column.id}>
+        <Droppable
+          droppableId={this.props.column.id}
+          isDropDisabled={this.props.isDropDisabled}
+          // type={this.props.column.id === "column-3" ? "done" : "active"}
+        >
           {(provided, snapshot) => (
             <TaskList
               ref={provided.innerRef}
