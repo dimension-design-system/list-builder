ec4d998669c7d4a54ee00e822a7b78cf16ecabac
James Lang <hi@thejameslang.com>
2023-08-21 13:41:11 -0500

    WIP

--- src/column.jsx
+++ src/column.jsx
@@ -4,13 +4,13 @@ import { Droppable } from "react-beautiful-dnd";
 import Task from "./task";
 
 const Container = styled.div`
-  margin: 8px;
-  border: 1px solid lightgrey;
-  border-radius: 2px;
-  width: 220px;
-
   display: flex;
   flex-direction: column;
+  width: 588px;
+  margin: 12px;
+  border: 1px solid var(--neutrals-grey-300, #e0e0e0);
+  border-radius: 10px;
+  padding: 24px;
 `;
 const Title = styled.h3`
   padding: 8px;
@@ -20,14 +20,55 @@ const TaskList = styled.div`
   transition: background-color 0.2s ease;
   background-color: ${(props) => (props.isDraggingOver ? "skyblue" : "white")};
   flex-grow: 1;
-  min-height: 100px;
+  min-height: 500px;
+`;
+const ActionBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-self: center;
+  width: 540px;
+  height: 32px;
+  padding: 4px 8px 4px 4px;
+  border-radius: 4px;
+  background: var(--neutrals-grey-100, #f5f5f5);
+`;
+const SelectAll = styled.div`
+  width: 100%;
+  padding: 4px 8px 4px 4px;
+`;
+const AddToGroupBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 export default class Column extends React.Component {
+  selectAll = false;
+
+  handleSelectAllChange = () => {
+    this.selectAll = !this.selectAll;
+  };
+
   render() {
     return (
       <Container>
         <Title>{this.props.column.title}</Title>
+        <ActionBar>
+          <input type="text"></input>
+          <button>filter</button>
+          <button>sort</button>
+        </ActionBar>
+        <pre>{"select all state " + this.selectAll}</pre>
+        <SelectAll>
+          <input
+            type="checkbox"
+            id="select-all"
+            name="select-all"
+            onChange={this.handleSelectAllChange}
+            checked={this.selectAll}
+          />
+          <label for="select-all">Select All Accounts</label>
+        </SelectAll>
         <Droppable
           droppableId={this.props.column.id}
           isDropDisabled={this.props.isDropDisabled}
@@ -46,6 +87,10 @@ export default class Column extends React.Component {
             </TaskList>
           )}
         </Droppable>
+        <AddToGroupBar>
+          <h3>0 Accounts Selected</h3>
+          <button>Add to Group</button>
+        </AddToGroupBar>
       </Container>
     );
   }
--- src/initial-data.js
+++ src/initial-data.js
@@ -1,29 +1,66 @@
 const initialData = {
   tasks: {
-    "task-1": { id: "task-1", content: "Take out the garbage" },
-    "task-2": { id: "task-2", content: "Watch my favorite show" },
-    "task-3": { id: "task-3", content: "Charge my phone" },
-    "task-4": { id: "task-4", content: "Cook dinner" },
+    "task-1": {
+      id: "task-1",
+      content: "Lorem ipsum",
+      accountName: "Analysis Checking",
+      accountNumber: "202049982",
+      companyName: "Brown Bear of San Francisco, CA",
+      currency: "USD",
+      accountType: "SVB-US",
+      selected: false,
+    },
+    "task-2": {
+      id: "task-2",
+      content: "dolor sit amet",
+      accountName: "Analysis Checking",
+      accountNumber: "202049982",
+      companyName: "Brown Bear of San Francisco, CA",
+      currency: "USD",
+      accountType: "SVB-US",
+      selected: false,
+    },
+    "task-3": {
+      id: "task-3",
+      content: "consectetur",
+      accountName: "Analysis Checking",
+      accountNumber: "202049982",
+      companyName: "Brown Bear of San Francisco, CA",
+      currency: "USD",
+      accountType: "SVB-US",
+      selected: false,
+    },
+    "task-4": {
+      id: "task-4",
+      content: "adipiscing",
+      accountName: "Analysis Checking",
+      accountNumber: "202049982",
+      companyName: "Brown Bear of San Francisco, CA",
+      currency: "USD",
+      accountType: "SVB-US",
+      selected: false,
+    },
   },
   columns: {
     "column-1": {
       id: "column-1",
-      title: "To do",
+      title: "Accounts",
       taskIds: ["task-1", "task-2", "task-3", "task-4"],
     },
     "column-2": {
       id: "column-2",
-      title: "In progress",
+      title: "Generic Group",
       taskIds: [],
     },
-    "column-3": {
-      id: "column-3",
-      title: "Done",
-      taskIds: [],
-    },
+    // "column-3": {
+    //   id: "column-3",
+    //   title: "Done",
+    //   taskIds: [],
+    // },
   },
   // Facilitate reordering of the columns
-  columnOrder: ["column-1", "column-2", "column-3"],
+  columnOrder: ["column-1", "column-2"],
+  // columnOrder: ["column-1", "column-2", "column-3"],
 };
 
 export default initialData;
