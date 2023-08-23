482a209ad9628f4d6e52f43e7d2946cfffb19d10
James Lang <hi@thejameslang.com>
2023-08-22 22:51:16 -0500

    style action bar

--- src/column.jsx
+++ src/column.jsx
@@ -31,14 +31,63 @@ const AccountList = styled.div`
   overflow-x: hidden;
 `;
 const ActionBar = styled.div`
+  width: -webkit-calc(100% - 12px);
+  width: -moz-calc(100% - 12px);
+  width: calc(100% - 12px);
   display: flex;
   justify-content: space-between;
   align-self: center;
-  width: 540px;
   height: 32px;
   padding: 4px 8px 4px 4px;
   border-radius: 4px;
   background: var(--neutrals-grey-100, #f5f5f5);
+`;
+const ActionBarSearch = styled.input`
+  display: flex;
+  flex: 1;
+  padding: 4px 8px 4px 36px;
+  border: none;
+  border-radius: 2px;
+  background: #fff
+    url("data:image/svg+xml,%3csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath fill-rule='evenodd' clip-rule='evenodd' d='M14.71 14H15.5L20.49 19L19 20.49L14 15.5V14.71L13.73 14.43C12.59 15.41 11.11 16 9.5 16C5.91 16 3 13.09 3 9.5C3 5.91 5.91 3 9.5 3C13.09 3 16 5.91 16 9.5C16 11.11 15.41 12.59 14.43 13.73L14.71 14ZM5 9.5C5 11.99 7.01 14 9.5 14C11.99 14 14 11.99 14 9.5C14 7.01 11.99 5 9.5 5C7.01 5 5 7.01 5 9.5Z' fill='%23757575'/%3e%3c/svg%3e")
+    no-repeat 8px center;
+  &::placeholder {
+    color: var(--neutrals-grey-500, #9e9e9e);
+    font-family: Roboto;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 20px;
+  }
+`;
+const ActionBarButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 0;
+  padding: 8px;
+  height: 30px;
+  color: var(--primary-primary-500, #007db8);
+  text-align: center;
+  font-family: Roboto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 14px;
+  border: none;
+  cursor: pointer;
+  background-color: inherit;
+  img {
+    width: 16px;
+    height: 16px;
+    flex-shrink: 0;
+  }
+`;
+const ActionBarFilter = styled(ActionBarButton)`
+  margin-left: 34px;
+`;
+const ActionBarSort = styled(ActionBarButton)`
+  margin-left: 12px;
 `;
 const SelectAll = styled.div`
   padding: 8px 0;
@@ -115,9 +164,18 @@ export default class Column extends React.Component {
           <TopHalf>
             <Title>{this.props.column.title}</Title>
             <ActionBar>
-              <input type="text"></input>
-              <button>filter</button>
-              <button>sort</button>
+              <ActionBarSearch
+                type="text"
+                placeholder="Search"
+              ></ActionBarSearch>
+              <ActionBarFilter>
+                <img src="data:image/svg+xml,%3csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cg clip-path='url(%23clip0_124_1537)'%3e%3cpath d='M2.83333 3.73984C4.18 5.4665 6.66666 8.6665 6.66666 8.6665V12.6665C6.66666 13.0332 6.96666 13.3332 7.33333 13.3332H8.66666C9.03333 13.3332 9.33333 13.0332 9.33333 12.6665V8.6665C9.33333 8.6665 11.8133 5.4665 13.16 3.73984C13.5 3.29984 13.1867 2.6665 12.6333 2.6665H3.36C2.80666 2.6665 2.49333 3.29984 2.83333 3.73984Z' fill='%23007DB8'/%3e%3c/g%3e%3cdefs%3e%3cclipPath id='clip0_124_1537'%3e%3crect width='16' height='16' fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e" />
+                Filter
+              </ActionBarFilter>
+              <ActionBarSort>
+                Sort
+                <img src="data:image/svg+xml,%3csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cg clip-path='url(%23clip0_124_1697)'%3e%3cpath d='M4.99996 6.3335L8.33329 9.66683L11.6666 6.3335H4.99996Z' fill='%23007DB8'/%3e%3c/g%3e%3cdefs%3e%3cclipPath id='clip0_124_1697'%3e%3crect width='16' height='16' fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e" />
+              </ActionBarSort>
             </ActionBar>
             <SelectAll>
               <SelectAllCheckbox
